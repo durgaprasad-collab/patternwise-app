2aac676494c0d11d724512a3cdf838d5d1d249fb
Durga Prasad <durgaprasad@patternwise.in>
2026-06-15 01:56:50 +0530

    Fix first chart creation in chart repository

--- app/astrology/services/chart_repository.py
+++ app/astrology/services/chart_repository.py
@@ -4,48 +4,56 @@ from app.models.chart_calculation import ChartCalculation
 
 class ChartRepository:
 
-    @staticmethod
-    def save_chart(
+   @staticmethod
+   def save_chart(
     profile_id,
     chart_system,
     ayanamsa,
     chart_json,
     profile_json=None
 ):
-        db = SessionLocal()
-        try:
-            existing_chart = (
+       db = SessionLocal()
+       try:
+
+        existing_chart = (
             db.query(ChartCalculation)
             .filter(
                 ChartCalculation.profile_id == profile_id,
                 ChartCalculation.chart_system == chart_system
             )
-            .order_by(
-                ChartCalculation.id.desc()
-                )
             .first()
+        )
+        if existing_chart:
+
+            existing_chart.chart_json = chart_json
+            existing_chart.profile_json = profile_json
+            existing_chart.ayanamsa = ayanamsa
+            existing_chart.version += 1
+
+            chart_record = existing_chart
+        else:
+            chart_record = ChartCalculation(
+                profile_id=profile_id,
+                chart_system=chart_system,
+                ayanamsa=ayanamsa,
+                chart_json=chart_json,
+                profile_json=profile_json,
+                version=1
             )
-            if existing_chart:
-                print("\nREPOSITORY RECEIVED:")
-                print(profile_json)
-                existing_chart.chart_json = chart_json
-                existing_chart.profile_json = profile_json
-                existing_chart.ayanamsa = ayanamsa
-                existing_chart.version += 1
 
-            db.commit()
-            db.refresh(existing_chart)
+            db.add(chart_record)
 
-            return existing_chart
-            
-        finally:
-            db.close()
-   
-    @staticmethod
-    def get_latest_chart(profile_id):
-        db = SessionLocal()
-        try:
-            return (
+        db.commit()
+        db.refresh(chart_record)
+
+        return chart_record
+       finally:
+        db.close()
+@staticmethod
+def get_latest_chart(profile_id):
+    db = SessionLocal()
+    try:
+           return (
             db.query(ChartCalculation)
             .filter(
                 ChartCalculation.profile_id == profile_id
@@ -55,5 +63,5 @@ class ChartRepository:
             )
             .first()
         )
-        finally:
+    finally:
             db.close()
